63eadd7e14240d3c1e0142b5f425ee15dec5a195
Yatin-Rana <yatinr965@gmail.com>
2025-03-24 17:34:19 +0530

    login component

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
 
-        <footer className="footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4 absolute bottom-0">
+        <footer className="footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4 fixed bottom-0">
             <aside>
                 <p>Copyright © {new Date().getFullYear()} - Made with 💗 by GeekishNoob</p>
             </aside>
